fa83a3ad3cfd9e948b1b9fbd205fd69c8d7fc0d4
WanfJunfei <wanfjunfei@WanfJunfeis-MacBook-Air.local>
2024-10-19 18:44:27 -0400

    asd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,6 @@ public class Main {
         System.out.println("git init");
         System.out.println("git add .");
         System.out.println("git commit -m \"Hello World\"");
-        System.out.println("");
+        System.out.println("aaaaa");
     }
 }
