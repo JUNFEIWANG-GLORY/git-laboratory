fbef453019661e70ba46fa1dacc9e043ce049b13
WanfJunfei <wanfjunfei@WanfJunfeis-MacBook-Air.local>
2024-10-19 17:57:35 -0400

    a

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Using this to practice git operations");
+        System.out.println("Push code to the repository");
+        System.out.println("git init");
+        System.out.println("git add .");
+        System.out.println("git commit -m \"Hello World\"");
+        System.out.println("git push origin main");
+    }
+}
